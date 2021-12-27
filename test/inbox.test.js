2b3040b66249a2ab8cf5ad66a4966feb39dfaa2f
Sanzay Dahal <sanzaydahal12345@gmail.com>
2021-12-27 12:28:10 +1100

    Testing with Mocha overview

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,3 +2,29 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+class Car {
+  park() {
+    return 'Stopped';
+  }
+
+  drive() {
+    return 'Vroom';
+  }
+}
+
+let car;
+
+beforeEach(() => {
+  car = new Car();
+});
+
+describe('Car', () => {
+  it('can park', () => {
+    assert.equal(car.park(), 'Stopped');
+  });
+
+  it('can drive', () => {
+    assert.equal(car.drive(), 'Vroom');
+  });
+});
