88a3c9265dc9119c76d1a9bfb10dab29cd375c35
Sanzay Dahal <sanzaydahal12345@gmail.com>
2021-12-27 19:25:26 +1100

    Update project to Solc v.0.8.11

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,7 +3,7 @@ const ganache = require('ganache-cli');
 const { describe } = require('mocha');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require('../compile');
+const { abi, evm } = require('../compile');
 
 let accounts;
 let inbox;
@@ -13,9 +13,9 @@ beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
   // Use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  inbox = await new web3.eth.Contract(abi)
     .deploy({
-      data: bytecode,
+      data: evm.bytecode.object,
       arguments: ['Hi there!'],
     })
     .send({ from: accounts[0], gas: '1000000' });
