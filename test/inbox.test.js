b49985cff763cfec3f9420cfa6d335432809eaa3
Sanzay Dahal <sanzaydahal12345@gmail.com>
2021-12-27 12:45:06 +1100

    Fetching Accounts from Ganache

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,30 +1,18 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
+const { describe } = require('mocha');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-class Car {
-  park() {
-    return 'Stopped';
-  }
-
-  drive() {
-    return 'Vroom';
-  }
-}
-
-let car;
-
 beforeEach(() => {
-  car = new Car();
+  // Get a list of all accounts
+  web3.eth.getAccounts().then((fetchedAccounts) => {
+    console.log(fetchedAccounts);
+  });
+
+  // Use one of those accounts to deploy the contract
 });
 
-describe('Car', () => {
-  it('can park', () => {
-    assert.equal(car.park(), 'Stopped');
-  });
-
-  it('can drive', () => {
-    assert.equal(car.drive(), 'Vroom');
-  });
+describe('Inbox', () => {
+  it('deploys a contract', () => {});
 });
