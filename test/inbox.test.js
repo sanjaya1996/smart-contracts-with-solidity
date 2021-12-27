7cbf11757c0fa679a7688fb907c5150d8cbe13e4
Sanzay Dahal <sanzaydahal12345@gmail.com>
2021-12-27 12:58:37 +1100

    Deployment with Web3

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,16 +3,26 @@ const ganache = require('ganache-cli');
 const { describe } = require('mocha');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-beforeEach(() => {
+let accounts;
+let inbox;
+
+beforeEach(async () => {
   // Get a list of all accounts
-  web3.eth.getAccounts().then((fetchedAccounts) => {
-    console.log(fetchedAccounts);
-  });
+  accounts = await web3.eth.getAccounts();
 
   // Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there!'],
+    })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
-  it('deploys a contract', () => {});
+  it('deploys a contract', () => {
+    console.log(inbox);
+  });
 });
